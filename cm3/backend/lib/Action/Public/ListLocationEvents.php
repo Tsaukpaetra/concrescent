03c9f6791a1b6f1f5d1277edd505e98454265c6a
Tsaukpaetra <htwiz2002@gmail.com>
2025-04-01 01:01:51 -0700

    Public events work
    Add modification header hints and handling
    Add special transform for the main website schedule grid format
    Include submission descriptions based on the first question that has "descri" in its title

--- cm3/backend/lib/Action/Public/ListLocationEvents.php
+++ cm3/backend/lib/Action/Public/ListLocationEvents.php
@@ -9,8 +9,11 @@ use CM3_Lib\database\Join;
 
 use CM3_Lib\models\eventinfo;
 use CM3_Lib\models\application\location;
+use CM3_Lib\models\application\locationcategory;
 use CM3_Lib\models\application\assignment;
 use CM3_Lib\models\application\submission;
+use CM3_Lib\models\forms\question;
+use CM3_Lib\models\forms\response;
 
 use CM3_Lib\util\CurrentUserInfo;
 
@@ -34,8 +37,11 @@ final class ListLocationEvents
         private Responder $responder,
         private eventinfo $eventinfo,
         private location $location,
+        private locationcategory $locationcategory,
         private assignment $assignment,
         private submission $submission,
+        private question $question,
+        private response $response,
         private CurrentUserInfo $CurrentUserInfo
     ) {
     }
@@ -51,50 +57,178 @@ final class ListLocationEvents
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, $params): ResponseInterface
     {
         $qp = $request->getQueryParams();
-        $override = $qp['override'] ?? null;
+        $excludeTimed = ($qp['excludeTimed'] ?? 'false') == 'true';
+        $excludeUntimed = ($qp['excludeUntimed'] ?? 'false') == 'true';
+        $transformToConventionMaster = ($qp['transformToConventionMaster'] ?? 'false') == 'true';
 
-        //TODO: Implement caching response for if-modified-since
+        $terms = [
+            $excludeTimed ? new SearchTerm('start_time', null, 'IS') : null,
+            $excludeUntimed ? new SearchTerm('start_time', null, 'IS NOT') : null,
+        ];
+
+        //Get modified date
+        $mod = $this->assignment->Search(new View([
+            'date_modified'
+        ], [
+            new Join($this->location, ['id' => 'location_id'], alias: 'l', subQSelectColumns: [
+                'id'
+            ], subQSearchTerms: [
+                new SearchTerm('event_id', $params['event_id']),
+                new SearchTerm('active', 1),
+            ])
+        ]), $terms, ['date_modified' => true], 1);
+
+        if ($mod === false)
+        {
+            //Skip everything, we have no events with the current query!
+            return $this->responder
+                ->withJson($response, []);
+        }
+
+        //Provide the HTTP-compliant modified time format
+        $dateTime = new \DateTime($mod[0]['date_modified']);
+        $dateTime->setTimezone(new \DateTimeZone('GMT'));
+        $mod[0]['date_modified'] = $dateTime->format('D, d M Y H:i:s \G\M\T');
+
+        $response = $response
+            ->withHeader('Last-Modified', $mod[0]['date_modified']);
+
+        //Check if we were asked about the modified time
+        if ($request->hasHeader('If-Modified-Since'))
+        {
+            if (new \DateTime($request->getHeaderLine('If-Modified-Since'), new \DateTimeZone('UTC')) >= $dateTime)
+            {
+                return $response
+                    ->withStatus(304);
+            }
+        }
+
 
         $order = array('short_code' => false);
 
+        //Fetch the question IDs that say Description for each context code and make searchterms out of them
+        $questionsearch = array_map(function($question){
+            return new SearchTerm('','',TermType:'OR',subSearch: [
+                new SearchTerm('question_id',$question['question_id']),
+                new SearchTerm('context_code',['A','S'], 'NOT IN')
+            ]);
+        }, $this->question->Search(new View(
+            ['event_id', new SelectColumn('id', Alias: 'question_id')],
+            [
+                new Join(
+                    $this->question,
+                    ['min_order' => 'order', 'context_code' => 'context_code'],
+                    'INNER',
+                    'q2',
+                    [
+                        new SelectColumn('context_code', true),
+                        new SelectColumn('order', false, 'MIN(?)', 'min_order')
+                    ],
+                    [
+                        new SearchTerm('event_id', $params['event_id']),
+                        new SearchTerm('context_code',['A','S'], 'NOT IN'),
+                        new SearchTerm('active', 1),
+                        new SearchTerm('title', '%descri%', 'LIKE'),
+                        new SearchTerm('type', 6,'>'),
+                    ]
+                    ),
+            ]
+        )));
+
 
         // Invoke the Domain with inputs and retain the result
-        $data =  $this->assignment->Search(new View([
-            'category_id','start_time','end_time',
-            new SelectColumn('real_name', JoinedTableAlias:'s'),
-            new SelectColumn('fandom_name', JoinedTableAlias:'s'),
-            new SelectColumn('name_on_badge', JoinedTableAlias:'s'),
-            new SelectColumn('application_status', JoinedTableAlias:'s'),
-            new SelectColumn('short_code', JoinedTableAlias:'l'),
-        ],[
-            new Join($this->location,['id'=>'location_id'],alias:'l', subQSelectColumns:[
-                'id','short_code'
-            ], subQSearchTerms:[
+        $data = $this->assignment->Search(new View([
+            'category_id',
+            'start_time',
+            'end_time',
+            new SelectColumn('real_name', JoinedTableAlias: 's'),
+            new SelectColumn('fandom_name', JoinedTableAlias: 's'),
+            new SelectColumn('name_on_badge', JoinedTableAlias: 's'),
+            new SelectColumn('application_status', JoinedTableAlias: 's'),
+            new SelectColumn('short_code', JoinedTableAlias: 'l'),
+            new SelectColumn('description',JoinedTableAlias:'desc')
+        ], [
+            new Join($this->location, ['id' => 'location_id'], alias: 'l', subQSelectColumns: [
+                'id',
+                'short_code'
+            ], subQSearchTerms: [
                 new SearchTerm('event_id', $params['event_id']),
                 new SearchTerm('active', 1),
             ]),
-            new Join($this->submission,['id'=>'application_id'],alias:'s')
-        ]),[
-        ], $order);
-
+            new Join($this->submission, ['id' => 'application_id'], alias: 's'),
+            new Join($this->response, [
+                'context_id' => new SearchTerm('id','',JoinedTableAlias:'s')
+            ],'left',alias:'desc',subQSelectColumns:[
+                new SelectColumn('response', Alias:'description'),
+                'context_id','context_code'
+                
+            ], subQSearchTerms:$questionsearch),
+        ]), $terms, $order);
+        
         //Add the display_name and blank the not-displayed parts
-        foreach ($data as &$value) {            
-            switch ($value['name_on_badge']) {
+        foreach ($data as &$value)
+        {
+            switch ($value['name_on_badge'])
+            {
                 case 'Fandom Name Large, Real Name Small':
-                    $value['display_name'] = trim(($value['fandom_name'] ??'') .' (' . $value['real_name'] . ')');
+                    $value['display_name'] = trim(($value['fandom_name'] ?? '') . ' (' . $value['real_name'] . ')');
                     break;
                 case 'Real Name Large, Fandom Name Small':
-                    $value['display_name'] = trim($value['real_name'] .' (' . ($value['fandom_name']??'') . ')');
+                    $value['display_name'] = trim($value['real_name'] . ' (' . ($value['fandom_name'] ?? '') . ')');
                     break;
                 case 'Fandom Name Only':
-                    $value['display_name'] = $value['fandom_name']??'';
+                    $value['display_name'] = $value['fandom_name'] ?? '';
                     $value['real_name'] = '';
                     break;
                 case 'Real Name Only':
                     $value['display_name'] = $value['real_name'];
                     $value['fandom_name'] = '';
-                break;
+                    break;
             }
+        }
+
+
+        if ($transformToConventionMaster)
+        {
+            //Fetch categories now too
+            $categories = $this->locationcategory->Search([
+                'id',
+                'color',
+                'name',
+                'description'
+            ], [
+                new SearchTerm('event_id', $params['event_id']),
+                new SearchTerm('active', 1),
+            ]);
+
+            $data = array_map(function ($location) use ($categories) {
+                //Get the associated category
+                $cat = array_values(array_filter($categories, function ($category) use ($location) {
+                    return $category['id'] == $location['category_id'];
+                }))[0] ?? [
+                    //Default unknown
+                    'id' => 0,
+                    'color' => '#2196F3',
+                    'name' => 'Unknown',
+                    'description' => 'Unknown'
+                ];
+
+                return [
+                    'title' => $location['real_name'],
+                    'start' => date(DATE_ISO8601, strtotime($location['start_time'])),
+                    'end' => date(DATE_ISO8601, strtotime($location['end_time'])),
+                    'resourceId' => $location['short_code'],
+
+                    'activityTypeTitle' => $cat['name'],
+                    'activityColour' => $cat['color'],
+                    'color' => $cat['color'],
+                    'activityType' => $cat['name'],
+
+
+                    'description' => $location['description'] ?? 'Description here',
+                    'roomName' => $location['name'],
+                ];
+            }, $data);
         }
 
 
--- cm3/backend/lib/Action/Public/ListLocations.php
+++ cm3/backend/lib/Action/Public/ListLocations.php
@@ -9,6 +9,7 @@ use CM3_Lib\database\Join;
 
 use CM3_Lib\models\eventinfo;
 use CM3_Lib\models\application\location;
+use CM3_Lib\models\application\assignment;
 
 use CM3_Lib\util\CurrentUserInfo;
 
@@ -32,6 +33,7 @@ final class ListLocations
         private Responder $responder,
         private eventinfo $eventinfo,
         private location $location,
+        private assignment $assignment,
         private CurrentUserInfo $CurrentUserInfo
     ) {
     }
@@ -47,7 +49,50 @@ final class ListLocations
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, $params): ResponseInterface
     {
         $qp = $request->getQueryParams();
-        $override = $qp['override'] ?? null;
+        $excludeTimed = ($qp['excludeTimed'] ?? 'false') == 'true';
+        $excludeUntimed = ($qp['excludeUntimed'] ?? 'false') == 'true';
+        $transformToConventionMaster = ($qp['transformToConventionMaster'] ?? 'false') == 'true';
+
+        //TODO: Implement caching response for if-modified-since
+        $terms = [
+            $excludeTimed ? new SearchTerm('start_time',null,'IS') : null,
+            $excludeUntimed ? new SearchTerm('start_time',null,'IS NOT') : null,
+        ];
+
+        //Get modified date
+        $mod = $this->location->Search(new View([
+            'date_modified'
+        ],[
+            new Join($this->assignment,['location_id'=>'id'],alias:'l', subQSelectColumns:[
+                new SelectColumn('location_id',true), new SelectColumn('id', EncapsulationFunction:'min(?)')
+            ], subQSearchTerms:$terms)
+        ]),[
+            new SearchTerm('event_id', $params['event_id']),
+            new SearchTerm('active', 1),
+        ], ['date_modified'=>true],1);
+
+        if($mod === false) {
+            //Skip everything, we have no events with the current query!
+            return $this->responder
+                ->withJson($response, []);
+        }
+        
+        //Provide the HTTP-compliant modified time format
+        $dateTime = new \DateTime($mod[0]['date_modified']);
+        $dateTime->setTimezone(new \DateTimeZone('GMT'));
+        $mod[0]['date_modified'] = $dateTime->format('D, d M Y H:i:s \G\M\T');
+
+        $response = $response
+        ->withHeader('Last-Modified', $mod[0]['date_modified']);
+        
+        //Check if we were asked about the modified time
+        if ($request->hasHeader('If-Modified-Since')) {
+            if (new \DateTime($request->getHeaderLine('If-Modified-Since'), new \DateTimeZone('UTC')) >= $dateTime) {
+                return $response
+                ->withStatus(304);
+            }
+        }
+        
         
         //TODO: Implement caching response for if-modified-since
 
@@ -60,10 +105,25 @@ final class ListLocations
 
 
         // Invoke the Domain with inputs and retain the result
-        $data = $this->location->Search([
+        $data = $this->location->Search(new View([
             'short_code', 'name','description'
-        ], $whereParts, $order);
+        ],[
+            new Join($this->assignment,['location_id'=>'id'],alias:'l', subQSelectColumns:[
+                new SelectColumn('location_id',true), new SelectColumn('id', EncapsulationFunction:'min(?)')
+            ], subQSearchTerms:$terms)
+        ]), $whereParts, $order);
 
+        if($transformToConventionMaster){
+            $data = array_map(function($location){
+                return [
+                    'id' => $location['short_code'],
+                    'resourceType' => 'Room',
+                    'roomName' => $location['name'],
+                    'title' => $location['name'],
+                    'description' => $location['description'],
+                ];
+            }, $data);
+        }
 
         // Build the HTTP response
         return $this->responder
